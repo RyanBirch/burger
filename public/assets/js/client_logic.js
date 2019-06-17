ff74af02eb0ae3a316ea15b8f822fc5cb00e8426
Ryan Birch <ryan.birch@gmail.com>
2019-06-17 00:17:38 -0400

    completed app functionality

--- public/assets/js/client_logic.js
+++ public/assets/js/client_logic.js
@@ -9,17 +9,15 @@ $('#burger-form').on('submit', function() {
         type: 'POST',
         data: burger
     })
-
-    // $.post('/api/burgers', burger)
 })
 
 
 $(document).on('click', '#eat', function() {
     let id = $(this).attr('data-id')
-    console.log('id: ' + id)
 
     $.ajax('/api/burgers/' + id, {
-        type: 'PUT',
+        type: 'PUT'
     })
-    .then(function() { location.reload() })
+    
+    location.reload() 
 })
