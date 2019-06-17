d07f2b47e4c1edf6f5a85077de6482142e116f3f
Ryan Birch <ryan.birch@gmail.com>
2019-06-17 02:01:00 -0400

    added styling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 // set port
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log('Listening on port ' + PORT))
 
 // data parsing
